0df477158b98aa19eab3f33e73ec16e3ba5a361f
Niels Henrik Hagen <nhhagen@comperiosearch.com>
2013-09-11 16:36:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vagrant ElasticSearch box
+
+Box to get an ElasticSearch environment quickly up and running in a VM.
+
+## Running the box
+
+1. Install [Vagrant](http://www.vagrantup.com/)
+2. `vagrant up`
+3. Open http://localhost:9200/
---
+++ manifests/default.pp
@@ -0,0 +1,59 @@
+define append_if_no_such_line($file, $line, $refreshonly = 'false') {
+   exec { "/bin/echo '$line' >> '$file'":
+      unless      => "/bin/grep -Fxqe '$line' '$file'",
+      path        => "/bin",
+      refreshonly => $refreshonly,
+   }
+}
+
+class must-have {
+  include apt
+  apt::ppa { "ppa:webupd8team/java": }
+
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+    before => Apt::Ppa["ppa:webupd8team/java"],
+  }
+
+  exec { 'apt-get update 2':
+    command => '/usr/bin/apt-get update',
+    require => [ Apt::Ppa["ppa:webupd8team/java"], Package["git-core"] ],
+  }
+
+  package { ["vim", "curl", "git-core", "bash"]:
+    ensure => present,
+    require => Exec["apt-get update"],
+    before => Apt::Ppa["ppa:webupd8team/java"],
+  }
+
+  package { "oracle-java7-installer":
+    ensure => present,
+    require => Exec["apt-get update 2"],
+  }
+
+  exec { "accept_license":
+    command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
+    cwd => "/home/vagrant",
+    user => "vagrant",
+    path => "/usr/bin/:/bin/",
+    require => Package["curl"],
+    before => Package["oracle-java7-installer"],
+    logoutput => true,
+  }
+
+  file { "/vagrant/elasticsearch":
+    ensure => directory,
+    before => Exec["download_elasticsearch"]
+  }
+
+  exec { "download_elasticsearch":
+    command => "curl -L https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.tar.gz | tar zx --directory=/vagrant/elasticsearch --strip-components 1",
+    cwd => "/vagrant",
+    user => "vagrant",
+    path => "/usr/bin/:/bin/",
+    require => Exec["accept_license"],
+    logoutput => true,
+  }
+}
+
+include must-have
